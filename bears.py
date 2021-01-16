72dba125d455329c9762a67f9cefa383e340cd05
Karina <knguy369@calpoly.edu>
2021-01-16 15:40:59 -0800

    add more conditions

--- bears.py
+++ bears.py
@@ -11,7 +11,7 @@ def bears(n):
      bears."""
      if n == 42: #base case
         return True
-     if n < 42: #base case
+     elif n < 42  : #base case
         return False
      elif n % 2 == 0 and bears(int(n / 2)) is True: #reduces n only if possible
             return bears(int(n / 2))
@@ -21,6 +21,10 @@ def bears(n):
         test_n = str(n)
         prod = int(test_n[-1]) * int(test_n[-2])
         test_n = int(test_n) - prod
+        if test_n < 42:
+            return False
+        if test_n == 42:
+            return True
         if bears(test_n) is True:
             return bears(test_n)
      return False #only if no other paths are possible
