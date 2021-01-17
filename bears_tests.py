dc55abaf96598c804916f6d39c6733aa714047b2
Karina <knguy369@calpoly.edu>
2021-01-16 17:32:45 -0800

    add tests cases for coverage

--- bears_tests.py
+++ bears_tests.py
@@ -2,17 +2,23 @@ import unittest
 from bears import *
 
 class TestAssign1(unittest.TestCase):
+    #determnes is bear function correctly returns True/False
     def test_bear_01(self):
         self.assertTrue(bears(250))
 
     def test_bear_02(self):
         self.assertTrue(bears(42))
+        self.assertTrue(bears(52))
+        self.assertTrue(bears(5200))
+        self.assertTrue(bears(7190))
 
     def test_bear_03(self):
         self.assertFalse(bears(53))
+        self.assertFalse(bears(5208))
 
     def test_bear_04(self):
         self.assertFalse(bears(41))
+        self.assertFalse(bears(708))
 
     def test_bear_05(self):
         self.assertFalse(bears(99))
