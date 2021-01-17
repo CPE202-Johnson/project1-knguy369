bc60a28dfbd08919690b4cff4d2a99905a39f121
Karina <knguy369@calpoly.edu>
2021-01-17 14:50:53 -0800

    change conditionals

--- bears.py
+++ bears.py
@@ -3,8 +3,6 @@
 int --> Boolean
 returns True if possible to win game with n bears and False otherwise
 """
-
-
 def bears(n):
     """A True return value means that it is possible to win
     the bear game by starting with n bears. A False return value means
@@ -16,21 +14,17 @@ def bears(n):
         return False
     elif n % 2 == 0 or n % 3 == 0 or n % 4 == 0 or n % 5 == 0:
         if n % 2 == 0:
-            test_n = n // 2
-            if bears(test_n) is True:
+            if bears( n // 2) is True:
                 return True
         if n % 5 == 0:
-            test_n = n - 42
-            if bears(test_n) is True:
+            if bears(n - 42) is True:
                 return True
-        if n % 3 or n % 4 == 0:
+        if n % 3 == 0  or n % 4 == 0:
             test_n = str(n)
             prod = int(test_n[-1]) * int(test_n[-2])
             test_n = n - prod
             if test_n < 42 or test_n == n: # if no change in n
                 return False
-            if test_n == 42:
-                return True
             if bears(test_n) is True:
                 return True
     return False
