8f7d9dcd359db2db92348fabba28880d6cfec829
Karina <knguy369@calpoly.edu>
2021-01-16 16:07:07 -0800

    add more conditions

--- bears.py
+++ bears.py
@@ -4,6 +4,8 @@ int --> Boolean
 returns True if possible to win game with n bears and False otherwise
 """
 
+
+
 def bears(n):
      """A True return value means that it is possible to win
      the bear game by starting with n bears. A False return value means
@@ -11,12 +13,12 @@ def bears(n):
      bears."""
      if n == 42: #base case
         return True
-     elif n < 42  : #base case
+     if n < 42  : #base case
         return False
      elif n % 2 == 0 and bears(int(n / 2)) is True: #reduces n only if possible
-            return bears(int(n / 2))
+           return True
      elif n % 5 == 0 and bears(n - 42) is True:
-            return bears(n - 42)
+           return True
      elif n % 3 or n % 4 == 0:
         test_n = str(n)
         prod = int(test_n[-1]) * int(test_n[-2])
@@ -26,8 +28,7 @@ def bears(n):
         if test_n == 42:
             return True
         if bears(test_n) is True:
-            return bears(test_n)
+            return True
      return False #only if no other paths are possible
-
 
 
