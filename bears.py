23617f9ffd1e65f907375bf5877e9fa1a39643c1
Karina <knguy369@calpoly.edu>
2021-01-16 17:09:59 -0800

    revert changes

--- bears.py
+++ bears.py
@@ -4,31 +4,34 @@ int --> Boolean
 returns True if possible to win game with n bears and False otherwise
 """
 
-
-
 def bears(n):
-     """A True return value means that it is possible to win
-     the bear game by starting with n bears. A False return value means
-     that it is not possible to win the bear game by starting with n
+    """A True return value means that it is possible to win
+    the bear game by starting with n bears. A False return value means
+    that it is not possible to win the bear game by starting with n
      bears."""
-     if n == 42: #base case
+    if n == 42: #base case
         return True
-     if n < 42  : #base case
+    if n < 42: #base case
         return False
-     elif n % 2 == 0 and bears(int(n / 2)) is True: #reduces n only if possible
-           return bears(int(n / 2))
-     elif n % 5 == 0 and bears(n - 42) is True:
-           return bears(n - 42)
-     elif n % 3 or n % 4 == 0:
-        test_n = str(n)
-        prod = int(test_n[-1]) * int(test_n[-2])
-        test_n = int(test_n) - prod
-        if test_n < 42:
-            return False
-        if test_n == 42:
-            return True
-        if bears(test_n) is True:
-            return bears(test_n)
-     return False #only if no other paths are possible
+    elif n % 2 == 0 or n % 3 == 0 or n % 4 == 0 or n % 5 == 0: #if possible to reduce
+        if n % 2 == 0:
+            test_n = int(n / 2)
+            if bears(test_n) is True:
+                return bears(test_n)
+        if n % 5 == 0:
+            test_n = n - 42
+            if bears(test_n) is True:
+                return bears(test_n)
+        if n % 3 or n % 4 == 0:
+            test_n = str(n)
+            prod = int(test_n[-1]) * int(test_n[-2])
+            test_n = n - prod
+            if test_n < 42 or test_n == n: # if no change in n
+                return False
+            if test_n == 42:
+                return True
+            if bears(test_n) is True:
+                return bears(test_n)
+    return False
 
 
